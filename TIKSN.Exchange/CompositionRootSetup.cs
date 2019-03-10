a8eb1535da30f3190e1568150ac01ec1ff50bed8
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-10 22:22:42 +0200

    use in memory data

--- TIKSN.Exchange/CompositionRootSetup.cs
+++ TIKSN.Exchange/CompositionRootSetup.cs
@@ -1,20 +1,27 @@
 ﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using TIKSN.DependencyInjection;
 using TIKSN.Finance.ForeignExchange;
+using TIKSN.Finance.ForeignExchange.Data;
+using TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore;
 
 namespace TIKSN.Exchange
 {
     public class CompositionRootSetup : AutofacCompositionRootSetupBase
     {
+        private static readonly InMemoryDatabaseRoot inMemoryDatabaseRoot = new InMemoryDatabaseRoot();
+
         public CompositionRootSetup(IConfigurationRoot configurationRoot) : base(configurationRoot)
         {
         }
 
         protected override void ConfigureContainerBuilder(ContainerBuilder builder)
         {
+            builder.RegisterType<ExchangeRateRepository>().As<IExchangeRateRepository>().InstancePerLifetimeScope();
             builder.RegisterType<ExchangeRateService>().As<IExchangeRateService>().InstancePerLifetimeScope();
             builder.RegisterType<TextLocalizer>().As<IStringLocalizer>().SingleInstance();
         }
@@ -25,6 +32,10 @@ namespace TIKSN.Exchange
 
         protected override void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ExchangeRatesContext>(options =>
+            {
+                options.UseInMemoryDatabase("ExchangeRates", inMemoryDatabaseRoot);
+            });
         }
     }
 }
