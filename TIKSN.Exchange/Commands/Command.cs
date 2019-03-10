d20b533f4de8e12a27b4d5de548923943e84ac0d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-10 20:21:12 +0200

    base command

---
+++ TIKSN.Exchange/Commands/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TIKSN.PowerShell;
+
+namespace TIKSN.Exchange.Commands
+{
+    public abstract class Command : CommandBase
+    {
+        private static readonly Lazy<IServiceProvider> rootServiceProvider = new Lazy<IServiceProvider>(CreateRootServiceProvider, false);
+
+        protected override IServiceProvider GetServiceProvider()
+        {
+            return rootServiceProvider.Value;
+        }
+
+        private static IServiceProvider CreateRootServiceProvider()
+        {
+            var configurationRootSetup = new ConfigurationRootSetup();
+            var configurationRoot = configurationRootSetup.GetConfigurationRoot();
+            var compositionRootSetup = new CompositionRootSetup(configurationRoot);
+            return compositionRootSetup.CreateServiceProvider();
+        }
+    }
+}
