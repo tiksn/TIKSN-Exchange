1d0deb7e3e641be958964c484ccea00f7d717fd8
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-06-23 21:28:03 +0300

    Update ConfigurationRootSetup.cs

--- TIKSN.Exchange/ConfigurationRootSetup.cs
+++ TIKSN.Exchange/ConfigurationRootSetup.cs
@@ -1,8 +1,19 @@
-﻿using TIKSN.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using TIKSN.Configuration;
 
 namespace TIKSN.Exchange
 {
     public class ConfigurationRootSetup : ConfigurationRootSetupBase
     {
+        protected override void SetupConfiguration(IConfigurationBuilder builder)
+        {
+            base.SetupConfiguration(builder);
+
+            builder.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { ConfigurationPath.Combine("ConnectionStrings", ConnectionStringNames.MainConnectionString), "Filename=database.db"}
+            });
+        }
     }
 }
