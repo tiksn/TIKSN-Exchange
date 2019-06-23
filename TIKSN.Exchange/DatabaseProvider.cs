2eb11822e3470791e9f0a7d37e1c3e20c4e23735
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-06-23 13:57:00 +0300

    Create DatabaseProvider.cs

---
+++ TIKSN.Exchange/DatabaseProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using TIKSN.Data.LiteDB;
+using TIKSN.FileSystem;
+
+namespace TIKSN.Exchange
+{
+    public class DatabaseProvider : LiteDbDatabaseProvider
+    {
+        public DatabaseProvider(IConfigurationRoot configuration, IKnownFolders knownFolders) : base(configuration, "Main", knownFolders.LocalAppData)
+        {
+        }
+    }
+}
